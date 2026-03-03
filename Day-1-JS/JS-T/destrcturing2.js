d60388e049119d01a401374cb2f68bc8c32fd31e
Mirudhula <mirudhula864@gmail.com>
2026-03-03 17:52:56 +0530

    updated files on destructuring

---
+++ Day-1-JS/JS-T/destrcturing2.js
@@ -0,0 +1,33 @@
+//Destrcturing
+
+
+//node  destrcturing2.js
+
+//1.array
+const value=[10,20,30];
+const [a, b, c]=value;
+console.log(a,b,c);
+
+//2.object
+const value2={
+  age:21,
+  address:{
+    ad1:"xyz",
+    ad2:"abc"
+  }
+};
+const {age , address:{ad1,ad2}} =value2;
+console.log(age,ad1,ad2);
+
+//short names
+const value3={
+  name3:"ravi",
+  age3:23,
+  address3:{
+    streetno:1,
+    ad3:"abc2"
+  }
+};
+const {name3:n , age3:aa, address3:{streetno:sn, ad3 :add}} =value3;
+console.log(n,aa,sn,add);
+
