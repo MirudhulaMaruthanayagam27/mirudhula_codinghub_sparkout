98f65d3e57d621b9a1b1561a9d2acee120b5492d
Mirudhula <mirudhula864@gmail.com>
2026-03-03 16:32:58 +0530

    add api files

---
+++ Day-1-JS/api/api1.js
@@ -0,0 +1,16 @@
+async function fetchuser(){
+  try{
+    console.log("fetching data");
+    const api_url=await fetch("https://jsonplaceholder.typicode.com/users");
+    const api_urldata=await api_url.json();
+    console.log("Fetched data:",api_urldata);
+  }
+  catch(error){
+    console.log("error:",error);
+  }
+  finally{
+    console.log("Fetched");
+  }
+  
+}
+fetchuser()
