3b400b2c05073fecf0e472b5247595734a37ef11
Mirudhula <mirudhula864@gmail.com>
2026-03-04 11:58:52 +0530

    add api files

---
+++ Day-1-JS/Api/call1.js
@@ -0,0 +1,44 @@
+//1
+function greet(name,callback){
+  callback(name);
+}
+function hello(name){
+  console.log("hi" +" " + name);
+}
+greet("Miru",hello);
+
+//2
+function add(a,b,callback){
+  callback(a+b);
+}
+add(2,3,function(res){
+  console.log(res);
+});
+
+//3
+function expo(a,b,callback){
+  callback(a^b);
+}
+expo(8,3,function(res){
+  console.log(res);
+});
+
+//4
+function user(callback){
+  const user={
+    nam:"abc",
+    age:20
+  }; 
+  callback(user);
+}
+user(function(res){
+  console.log(user.name);
+});
+
+//5
+function fullname(frst,last, callback){
+  callback(frst+ " " + last);
+}
+fullname("miru","miru s",function(name){
+  console.log(name);
+});
