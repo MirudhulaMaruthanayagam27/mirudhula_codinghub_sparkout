0ff2181d96afcc36e65b8db7ce94c0845df0c630
Mirudhula <mirudhula864@gmail.com>
2026-03-04 13:21:50 +0530

    modified this file

--- Day-1-JS/JS-T/api/promise3.js
+++ Day-1-JS/JS-T/api/promise3.js
@@ -9,5 +9,24 @@ function checkage(age){
     }
   });
 }
-checkage(20).then(res => console.log(res))
+checkage(20)
+.then(res => console.log(res))
+.catch(err => console.log(err));
+
+//login system
+function login(name,pass){
+  return new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      if(name=="miru" && pass==123){
+        resolve("Logged In");
+      }
+      else{
+        reject("Logout");
+      }
+    },2000);
+    
+  })
+}
+login("miru",123)
+.then(out => console.log(out))
 .catch(err => console.log(err));
